335a0eb6e5b12d8f483e737f4415f403e7352ec3
Dion Singh <55777067+DionMS8@users.noreply.github.com>
2022-04-21 00:59:10 -0400

    Created a Room ID using the UUID Library

---
+++ plan.md
@@ -0,0 +1,11 @@
+# Plan of Action
+
+- Initialize the First View
+- Create a Room ID
+- Add Ability to View Webcam View
+- Add Styling
+- Add Messaging Feature
+- Add Mute Button
+- Add Stop Video Button
+
+TOOLS: Socket.io, Peer.js, WebRTC, Express, Nodemon
--- server.js
+++ server.js
@@ -1,13 +1,38 @@
+// IMPORTING THE EXPRESS FRAMEWORK
+const express = require("express");
+// CREATING AN EXPRESS APP
+const app = express();
+
+// LOADING AN HTTP SERVER
+const server = require('http').Server(app);
+
+// IMPORTING THE UUID LIBRARY
+const { v4: uuidv4 } = require('uuid');
+
+// SETTING THE EJS VIEW ENGINE
+app.set('view engine', 'ejs');
 
 
+// CREATING EXPRESS ROUTES
+
+// ROUTE FOR THE ROOT PATH
+app.get('/', (req, res) => {
+    // res.status(200).send("Hello World");
+    res.redirect(`/${uuidv4()}`);
+})
+
+// CREATING A URL TO INCLUDE THE ROOM UUID
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room })
+})
 
 
+// SETTING THE SERVER TO LISTEN ON LOCALHOST PORT 3030
+server.listen(3030);
 
-
-
-
-
-
+// NOTE: HTTP STATUS CODE: 200 (OK) MEANS 
+// THAT THE REQUEST WAS RECEIVED, UNDERSTOOD, 
+// AND IS BEING PROCESSED
 
 
 
